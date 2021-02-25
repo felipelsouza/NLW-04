c8b26cd3a5572c8746888f5e493faa42781a7cac
Felipe Souza <felipelsouza.dev@gmail.com>
2021-02-25 01:14:42 -0300

    Create integration tests

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,30 +1,30 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
-    async create(req: Request, res: Response) {
-        const { name, email } = req.body;
+  async create(req: Request, res: Response) {
+    const { name, email } = req.body;
 
-        const usersRepository = getRepository(User);
+    const usersRepository = getCustomRepository(UsersRepository);
 
-        const userAlredyExists = await usersRepository.findOne({
-            email
-        });
+    const userAlredyExists = await usersRepository.findOne({
+      email,
+    });
 
-        if(userAlredyExists) {
-            return res.status(400).json({ error: 'User alredy exists!' });
-        }
-
-        const user = usersRepository.create({
-            name,
-            email
-        });
-
-        await usersRepository.save(user);
-
-        return res.json(user);
+    if (userAlredyExists) {
+      return res.status(400).json({ error: 'User alredy exists!' });
     }
+
+    const user = usersRepository.create({
+      name,
+      email,
+    });
+
+    await usersRepository.save(user);
+
+    return res.status(201).json(user);
+  }
 }
 
 export { UserController };
